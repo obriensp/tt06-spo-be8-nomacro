38e5b9babb1802de3ff51d8f983741b919f71d3c
Sean Patrick O'Brien <sean.patrick.obrien@me.com>
2024-03-23 13:16:07 -0700

    Debugger microcode area test

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_register (
+module tt_um_debugger_microcode (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,18 +16,34 @@ module tt_um_register (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  register u_reg(
-    .CLK(clk),
-    .RESETn(rst_n),
-
-    .CLR(uio_in[0]),
-
-    .DIN(ui_in),
-    .DOUT(uo_out),
-    .LOADn(uio_in[1])
+  wire [18:0] outputs;
+  debugger_microcode dmc(
+    .ADDR(ui_in[4:0]),
+    .WRITE(ui_in[5]),
+    .STEP(ui_in[7:6]),
+    .PREADY(outputs[18]),
+    .PADDR_OR_PWDATA(outputs[17]),
+    .OUTREG_OR_BUS(outputs[16]),
+    .D_AIn(outputs[15]),
+    .D_BIn(outputs[14]),
+    .D_OIn(outputs[13]),
+    .D_IIn(outputs[12]),
+    .D_Jn(outputs[11]),
+    .D_FIn(outputs[10]),
+    .D_MIn(outputs[9]),
+    .D_RI(outputs[8]),
+    .D_DOn(outputs[7]),
+    .D_AOn(outputs[6]),
+    .D_BOn(outputs[5]),
+    .D_IOn(outputs[4]),
+    .D_COn(outputs[3]),
+    .D_EOn(outputs[2]),
+    .D_ROn(outputs[1]),
+    .D_NOn(outputs[0])
   );
 
-  assign uio_out = 8'b0;
-  assign uio_oe  = 8'b0;
+  assign uo_out = outputs[7:0];
+  assign uio_out = outputs[15:8];
+  assign uio_oe = {outputs[18:16], 5'b0};
 
 endmodule
