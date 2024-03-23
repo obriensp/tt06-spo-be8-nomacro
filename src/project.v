50b9aa69b71fb4b85e563a865324a7e4ca3b62fb
Sean Patrick O'Brien <sean.patrick.obrien@me.com>
2024-03-22 22:27:23 -0700

    i2c-slave area test

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_i2c_to_apb (
+module tt_um_i2c_slave (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,36 +15,21 @@ module tt_um_i2c_to_apb (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-  /*
-  module i2c_to_apb(
-    input wire CLK,
-    input wire RESETn,
+  i2c_slave i2cs(
+    .clk(clk),
+    .rst(~rst_n),
 
-    input  wire        scl_i,
-    output wire        scl_o,
-    output wire        scl_t,
-    input  wire        sda_i,
-    output wire        sda_o,
-    output wire        sda_t,
+    .release_bus(1'b0),
 
-    input wire   [6:0] device_address,
-    input wire   [6:0] device_address_mask,
+    .s_axis_data_tdata(ui_in),
+    .s_axis_data_tvalid(uio_in[0]),
+    .s_axis_data_tready(uio_out[1]),
+    .s_axis_data_tlast(uio_in[4]),
 
-    output wire        PSEL,
-    output wire  [7:0] PADDR,
-    output wire        PENABLE,
-    output wire        PWRITE,
-    output wire  [7:0] PWDATA,
-    input wire   [7:0] PRDATA,
-    input wire         PREADY
-  );
-  */
-
-  wire [7:0] paddr;
-  wire [7:0] pwdata;
-  i2c_to_apb u_i2c(
-    .CLK(clk),
-    .RESETn(rst_n),
+    .m_axis_data_tdata(uo_out),
+    .m_axis_data_tvalid(uio_out[5]),
+    .m_axis_data_tready(uio_in[6]),
+    .m_axis_data_tlast(uio_out[7]),
 
     .scl_i(uio_in[2]),
     .scl_o(uio_out[2]),
@@ -54,39 +39,34 @@ module tt_um_i2c_to_apb (
     .sda_o(uio_out[3]),
     .sda_t(uio_oe[3]),
 
-    .device_address(7'd42),
-    .device_address_mask(7'h7F),
+    .busy(),
+    .bus_address(),
+    .bus_addressed(),
+    .bus_active(),
 
-    .PSEL(uio_out[0]),
-    .PADDR(paddr),
-    .PENABLE(uio_out[1]),
-    .PWRITE(uio_out[4]),
-    .PWDATA(pwdata),
-    .PRDATA(ui_in),
-    .PREADY(uio_in[5])
+    .enable(1'b1),
+    .device_address(7'd42),
+    .device_address_mask(7'h7F)
   );
 
-  assign uio_oe[0] = 1'b1;
+  /*
+  .s_axis_data_tvalid(uio_in[0]),
+  .s_axis_data_tready(uio_out[1]),
+  .s_axis_data_tlast(uio_in[4]),
+  .m_axis_data_tvalid(uio_out[5]),
+  .m_axis_data_tready(uio_in[6]),
+  .m_axis_data_tlast(uio_out[7]),
+  */
+
+  assign uio_oe[0] = 1'b0;
   assign uio_oe[1] = 1'b1;
-  // 2 and 3 are SCL/SDA
-  assign uio_oe[4] = 1'b1;
-  assign uio_oe[5] = 1'b0;
+  assign uio_oe[4] = 1'b0;
+  assign uio_oe[5] = 1'b1;
   assign uio_oe[6] = 1'b0;
-  assign uio_oe[7] = 1'b0;
+  assign uio_oe[7] = 1'b1;
 
-  assign uio_out[5] = 1'b0;
+  assign uio_out[0] = 1'b0;
+  assign uio_out[4] = 1'b0;
   assign uio_out[6] = 1'b0;
-  assign uio_out[7] = 1'b0;
-
-  reg toggle;
-  always @(posedge clk)
-    begin
-      if (~rst_n)
-        toggle <= 1'b0;
-      else
-        toggle <= ~toggle;
-    end
-
-  assign uo_out = toggle ? paddr : pwdata;
 
 endmodule
