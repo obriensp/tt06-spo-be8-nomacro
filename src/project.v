d4b5d65d7d2b0e9cbb3c1e39ca18fb8b719802b4
Sean Patrick O'Brien <sean.patrick.obrien@me.com>
2024-03-22 21:12:49 -0700

    i2c-to-apb area test

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_core (
+module tt_um_i2c_to_apb (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,62 +15,78 @@ module tt_um_core (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-  // module core(
-  //   input wire        CLK,
-  //   input wire        RESETn,
-  //
-  //   input wire        DEBUG_REQUEST,
-  //   output wire       DEBUG_ACK,
-  //   input wire  [7:0] DEBUG_DATA,
-  //   input wire        D_CLR, D_HLT, D_CE, D_SU,
-  //   input wire        D_AIn, D_BIn, D_OIn, D_IIn, D_Jn, D_FIn, D_MIn, D_RI,
-  //   input wire        D_DOn, D_AOn, D_BOn, D_IOn, D_COn, D_EOn, D_ROn, D_NOn,
-  //
-  //   output wire [7:0] BUS,
-  //   input wire  [7:0] INREG,
-  //   output wire [7:0] OUTREG,
-  //   output wire       HALTED
-  // )
+  /*
+  module i2c_to_apb(
+    input wire CLK,
+    input wire RESETn,
 
-  wire debug_ack;
-  wire [7:0] bus;
-  core u_core(
+    input  wire        scl_i,
+    output wire        scl_o,
+    output wire        scl_t,
+    input  wire        sda_i,
+    output wire        sda_o,
+    output wire        sda_t,
+
+    input wire   [6:0] device_address,
+    input wire   [6:0] device_address_mask,
+
+    output wire        PSEL,
+    output wire  [7:0] PADDR,
+    output wire        PENABLE,
+    output wire        PWRITE,
+    output wire  [7:0] PWDATA,
+    input wire   [7:0] PRDATA,
+    input wire         PREADY
+  );
+  */
+
+  wire [7:0] paddr;
+  wire [7:0] pwdata;
+  i2c_to_apb u_i2c(
     .CLK(clk),
     .RESETn(rst_n),
 
-    .DEBUG_REQUEST(1'b0),
-    .DEBUG_ACK(debug_ack),
-    .DEBUG_DATA(8'b0),
+    .scl_i(uio_in[2]),
+    .scl_o(uio_out[2]),
+    .scl_t(uio_oe[2]),
 
-    .D_CLR(1'b0),
-    .D_HLT(1'b0),
-    .D_CE(1'b0),
-    .D_SU(1'b0),
-    .D_AIn(1'b0),
-    .D_BIn(1'b0),
-    .D_OIn(1'b0),
-    .D_IIn(1'b0),
-    .D_Jn(1'b0),
-    .D_FIn(1'b0),
-    .D_MIn(1'b0),
-    .D_RI(1'b0),
-    .D_DOn(1'b0),
-    .D_AOn(1'b0),
-    .D_BOn(1'b0),
-    .D_IOn(1'b0),
-    .D_COn(1'b0),
-    .D_EOn(1'b0),
-    .D_ROn(1'b0),
-    .D_NOn(1'b0),
+    .sda_i(uio_in[3]),
+    .sda_o(uio_out[3]),
+    .sda_t(uio_oe[3]),
 
-    .BUS(bus),
-    .INREG(ui_in),
-    .OUTREG(uo_out),
-    .HALTED(uio_out[0])
+    .device_address(7'd42),
+    .device_address_mask(7'h7F),
+
+    .PSEL(uio_out[0]),
+    .PADDR(paddr),
+    .PENABLE(uio_out[1]),
+    .PWRITE(uio_out[4]),
+    .PWDATA(pwdata),
+    .PRDATA(ui_in),
+    .PREADY(uio_in[5])
   );
 
-  assign uio_out[7:1] = 7'b0;
   assign uio_oe[0] = 1'b1;
-  assign uio_oe[7:1] = 7'b0;
+  assign uio_oe[1] = 1'b1;
+  // 2 and 3 are SCL/SDA
+  assign uio_oe[4] = 1'b1;
+  assign uio_oe[5] = 1'b0;
+  assign uio_oe[6] = 1'b0;
+  assign uio_oe[7] = 1'b0;
+
+  assign uio_out[5] = 1'b0;
+  assign uio_out[6] = 1'b0;
+  assign uio_out[7] = 1'b0;
+
+  reg toggle;
+  always @(posedge clk)
+    begin
+      if (~rst_n)
+        toggle <= 1'b0;
+      else
+        toggle <= ~toggle;
+    end
+
+  assign uo_out = toggle ? paddr : pwdata;
 
 endmodule
