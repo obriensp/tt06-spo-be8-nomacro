abdf49a8fe051e6cdc4cdf317021b229eaa5a833
Sean Patrick O'Brien <sean.patrick.obrien@me.com>
2024-03-23 10:14:25 -0700

    Ram area test

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_i2c_slave (
+module tt_um_ram (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,58 +15,16 @@ module tt_um_i2c_slave (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-  i2c_slave i2cs(
-    .clk(clk),
-    .rst(~rst_n),
-
-    .release_bus(1'b0),
-
-    .s_axis_data_tdata(ui_in),
-    .s_axis_data_tvalid(uio_in[0]),
-    .s_axis_data_tready(uio_out[1]),
-    .s_axis_data_tlast(uio_in[4]),
-
-    .m_axis_data_tdata(uo_out),
-    .m_axis_data_tvalid(uio_out[5]),
-    .m_axis_data_tready(uio_in[6]),
-    .m_axis_data_tlast(uio_out[7]),
-
-    .scl_i(uio_in[2]),
-    .scl_o(uio_out[2]),
-    .scl_t(uio_oe[2]),
-
-    .sda_i(uio_in[3]),
-    .sda_o(uio_out[3]),
-    .sda_t(uio_oe[3]),
-
-    .busy(),
-    .bus_address(),
-    .bus_addressed(),
-    .bus_active(),
-
-    .enable(1'b1),
-    .device_address(7'd42),
-    .device_address_mask(7'h7F)
+  ram u_ram(
+    .CLK(clk),
+    .RESETn(rst_n),
+    .ADDR(uio_in[3:0]),
+    .DIN(ui_in),
+    .DOUT(uo_out),
+    .RI(uio_in[4])
   );
 
-  /*
-  .s_axis_data_tvalid(uio_in[0]),
-  .s_axis_data_tready(uio_out[1]),
-  .s_axis_data_tlast(uio_in[4]),
-  .m_axis_data_tvalid(uio_out[5]),
-  .m_axis_data_tready(uio_in[6]),
-  .m_axis_data_tlast(uio_out[7]),
-  */
-
-  assign uio_oe[0] = 1'b0;
-  assign uio_oe[1] = 1'b1;
-  assign uio_oe[4] = 1'b0;
-  assign uio_oe[5] = 1'b1;
-  assign uio_oe[6] = 1'b0;
-  assign uio_oe[7] = 1'b1;
-
-  assign uio_out[0] = 1'b0;
-  assign uio_out[4] = 1'b0;
-  assign uio_out[6] = 1'b0;
+  assign uio_out = 8'b0;
+  assign uio_oe = 8'b0;
 
 endmodule
