214f73ce8993b11ef883aebccd2c618c387a8da1
Sean Patrick O'Brien <sean.patrick.obrien@me.com>
2024-03-30 00:41:29 -0700

    Layout tweaks

--- odb_power.py
+++ odb_power.py
@@ -48,20 +48,20 @@ def power(reader, macro_x_pos: float):
     odb.dbSBox_create(vgnd_wire, met4, x, ymin, x + 1600, ymax, "STRIPE")
     odb.dbBox_create(vgnd_bpin, met4, x, ymin, x + 1600, ymax)
 
-  ymin = 10000
-  ymax = ymin + 45000
-
-  x_vpwr = 133400 - 3100 # 18280
-  x_vgnd = x_vpwr + 2300 + 460 # 95080
-
-  for i in range(1):
-    x = int(macro_x_pos * 1000 + x_vpwr + i * 153600)
-    odb.dbSBox_create(vpwr_wire, met4, x, ymin, x + 1600, ymax, "STRIPE")
-    odb.dbBox_create(vpwr_bpin, met4, x, ymin, x + 1600, ymax)
-  for i in range(1):
-    x = int(macro_x_pos * 1000 + x_vgnd + i * 153600)
-    odb.dbSBox_create(vgnd_wire, met4, x, ymin, x + 1600, ymax, "STRIPE")
-    odb.dbBox_create(vgnd_bpin, met4, x, ymin, x + 1600, ymax)
+#   ymin = 10000
+#   ymax = ymin + 45000
+#
+#   x_vpwr = 133400 - 3100 # 18280
+#   x_vgnd = x_vpwr + 2300 + 460 # 95080
+#
+#   for i in range(1):
+#     x = int(macro_x_pos * 1000 + x_vpwr + i * 153600)
+#     odb.dbSBox_create(vpwr_wire, met4, x, ymin, x + 1600, ymax, "STRIPE")
+#     odb.dbBox_create(vpwr_bpin, met4, x, ymin, x + 1600, ymax)
+#   for i in range(1):
+#     x = int(macro_x_pos * 1000 + x_vgnd + i * 153600)
+#     odb.dbSBox_create(vgnd_wire, met4, x, ymin, x + 1600, ymax, "STRIPE")
+#     odb.dbBox_create(vgnd_bpin, met4, x, ymin, x + 1600, ymax)
 
 
 if __name__ == "__main__":
